ba0fd153352dc3716ae7cabe8e4a20fa669b3e43
Julien Verneaut <jverneaut@gmail.com>
2025-07-02 18:34:22 +0200

    Remove useless product creation

--- app/routes/webhooks.inventory_levels.update.jsx
+++ app/routes/webhooks.inventory_levels.update.jsx
@@ -30,48 +30,48 @@ export async function action({ request }) {
   //   },
   // );
 
-  const response = await admin.graphql(
-    `#graphql
-    mutation createProduct($product: ProductCreateInput!) {
-      productCreate(product: $product) {
-        product {
-          id
-          title
-          handle
-          status
-          variants(first: 10) {
-            edges {
-              node {
-                id
-                price
-                barcode
-                createdAt
-              }
-            }
-          }
-        }
-      }
-    }
-   `,
-    {
-      variables: {
-        product: {
-          title: `TEST Product creation`,
-        },
-      },
-    },
-  );
+  // const response = await admin.graphql(
+  //   `#graphql
+  //   mutation createProduct($product: ProductCreateInput!) {
+  //     productCreate(product: $product) {
+  //       product {
+  //         id
+  //         title
+  //         handle
+  //         status
+  //         variants(first: 10) {
+  //           edges {
+  //             node {
+  //               id
+  //               price
+  //               barcode
+  //               createdAt
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  //  `,
+  //   {
+  //     variables: {
+  //       product: {
+  //         title: `TEST Product creation`,
+  //       },
+  //     },
+  //   },
+  // );
 
-  const responseJson = await response.json();
-  const product = responseJson.data.productCreate.product;
-  const variantId = product.variants.edges[0].node.id;
+  // const responseJson = await response.json();
+  // const product = responseJson.data.productCreate.product;
+  // const variantId = product.variants.edges[0].node.id;
 
-  console.log({
-    product,
-    variantId,
-  });
+  // console.log({
+  //   product,
+  //   variantId,
+  // });
 
-  console.log("Product created!");
+  // console.log("Product created!");
 
   return new Response();
 }
