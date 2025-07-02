2fd96424ff335656226c9ece62d8047e60a285af
Julien Verneaut <jverneaut@gmail.com>
2025-07-02 18:33:59 +0200

    Log payload and add warning

--- app/routes/webhooks.inventory_levels.update.jsx
+++ app/routes/webhooks.inventory_levels.update.jsx
@@ -1,7 +1,34 @@
 import { authenticate } from "../shopify.server";
 
 export async function action({ request }) {
-  const { admin } = await authenticate.webhook(request);
+  const { admin, payload } = await authenticate.webhook(request);
+
+  console.log(payload);
+
+  // TODO:
+  // I think we should be very careful about how we handle setting the profile,
+  // as it can change between the moment the person added the last item to cart
+  // and the moment he goes to checkout.
+  // If there is only one item remaining, then he can order it, then the item is
+  // out of stock, then the new shipping profile is applied and as such the order
+  // will be displayed as shipping later
+
+  // await admin.graphql(
+  //   `mutation assignToBackorderProfile($id: ID!, $variants: [ID!]!) {
+  //     deliveryProfileUpdate(id: $id, profile: {
+  //       variantsToAssociate: $variants
+  //     }) {
+  //       profile { id }
+  //       userErrors { message }
+  //     }
+  //   }`,
+  //   {
+  //     variables: {
+  //       id: BACKORDER_PROFILE_ID,
+  //       variants: [variantId],
+  //     },
+  //   },
+  // );
 
   const response = await admin.graphql(
     `#graphql
