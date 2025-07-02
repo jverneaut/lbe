9340f85ed70e7f9c3fea2892c18f869b3d2df2e1
Julien Verneaut <jverneaut@gmail.com>
2025-07-02 17:19:05 +0200

    Remove try catch

--- dbsetup.js
+++ dbsetup.js
@@ -6,7 +6,6 @@ import fs from 'node:fs'
 
 const env = { ...process.env }
 
-try {
 // place Sqlite3 database on volume
 const source = path.resolve('/dev.sqlite')
 const target = '/data/' + path.basename(source)
@@ -14,10 +13,6 @@ if (!fs.existsSync(source) && fs.existsSync('/data')) fs.symlinkSync(target, sou
 const newDb = !fs.existsSync(target)
 if (newDb && process.env.BUCKET_NAME) {
   await exec(`npx litestream restore -config litestream.yml -if-replica-exists ${target}`)
-}
-}
-catch (err) {
-  console.log(err);
 }
 
 // prepare database
